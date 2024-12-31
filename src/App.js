178d05b22dc7744339653244b194c29158a7a5d4
eloy-lee <kh.lee@pet-friends.co.kr>
2024-12-31 20:58:11 +0900

    feat: test component 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { TestComponent } from "./components/test-component";
+import { TestComponent2 } from "./components/test-component2";
+
 function App() {
+  console.log("app이 재호출되었습니다.");
   return (
     <div className="App">
-      <div>hello</div>
+      <TestComponent />
+      <TestComponent2 />
     </div>
   );
 }
